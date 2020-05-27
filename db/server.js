1e88b99fff7ae918709cef5c5bd662c394e63eec
prince naija <princeachuru@gmail.com>
2020-05-27 18:42:37 -0400

    routes to the frontend

--- db/server.js
+++ db/server.js
@@ -11,3 +11,11 @@ var PORT = process.env.port || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
