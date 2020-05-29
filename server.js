483878dec590a7186a630ae99d224348d56bde26
prince naija <princeachuru@gmail.com>
2020-05-28 22:23:00 -0400

    html file links fixed, notId var added to index.js

--- server.js
+++ server.js
@@ -2,13 +2,15 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
-
+// var fs = require("fs");
+var db = require("./db.json");
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.port || 3000;
 
 // Sets up the Express app to handle data parsing
+app.use("/assets", express.static("./assets/"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -19,6 +21,30 @@ app.get("/", function (req, res) {
 app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "notes.html"));
 });
+app.get("/api/notes", function (req, res) {
+  // Should read the`db.json` file and
+  // return all saved notes as JSON.
+  console.log(db);
+  res.json(db);
+});
+app.post("/api/notes", function (req, res) {
+  // Should receive a new note to save on the request body
+  console.log(req.body);
+
+  //  add it to the`db.json` file, and
+  // fs? but first push?
+  // then return the new note to the client
+  // temp
+  // db.push(req.body);
+  // res.json(db);
+});
+// app.delete("/api/notes/:id", function (req, res) {
+//   // Should receive a query parameter containing the id of a note to delete.
+//   // you'll need to find a way to give each note a unique `id` when it's saved.
+//   // In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property
+//   // then rewrite the notes to the`db.json` file
+// });
+
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
