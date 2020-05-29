189a92497d804091593fbe7b62d2a1a2c9effb5a
prince naija <princeachuru@gmail.com>
2020-05-29 10:15:08 -0400

    removed modules

--- server.js
+++ server.js
@@ -28,12 +28,18 @@ app.post("/api/notes", function (req, res) {
   db.push(req.body);
   res.json(db);
 });
-// app.delete("/api/notes/:id", function (req, res) {
-//   // Should receive a query parameter containing the id of a note to delete.
-//   // you'll need to find a way to give each note a unique `id` when it's saved.
-//   // In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property
-//   // then rewrite the notes to the`db.json` file
-// });
+app.delete("/api/notes/:id", function (req, res) {
+  let noteId = req.params.id;
+  // console.log(noteId);
+  let removeId;
+  console.log(removeId);
+  for (let i = 0; i < db.length; i++) {
+    if (db[i].id === noteId) {
+      removeId = i;
+    }
+  }
+  db.splice(removeId, 1);
+});
 
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
